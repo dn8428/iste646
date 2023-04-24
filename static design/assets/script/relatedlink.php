8675cb4743f971ddd9b6353bef1ad24798e797a5
tmps880305 <tmps880305@gmail.com>
2023-04-24 17:27:43 -0400

    Latest update with navbar

--- static design/assets/script/relatedlink.php
+++ static design/assets/script/relatedlink.php
@@ -10,10 +10,12 @@ $_SESSION['rootPath'] = '../../';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="<?php echo $_SESSION['rootPath']; ?>assets/css/style.css"/>
     <link rel="stylesheet" type="text/css"
           href="<?php echo $_SESSION['rootPath']; ?>assets/css/mastermind-main-page.css"/>
-    <link rel="stylesheet" type="text/css" href="<?php echo $_SESSION['rootPath']; ?>assets/css/mastermind-footer.css"/>
+    <!--    <link rel="stylesheet" type="text/css" href="-->
+    <?php //echo $_SESSION['rootPath']; ?><!--assets/css/mastermind-footer.css"/>-->
     <script src="<?php echo $_SESSION['rootPath']; ?>assets/js/main.js"></script>
 
 
@@ -99,11 +101,10 @@ $_SESSION['rootPath'] = '../../';
 </div>
 
 
-<footer class="footer">
-    <div>
-        <a>Thank you !</a>
-    </div>
-</footer>
+<?php
+include($_SESSION['rootPath'] . "assets/inc/footer.php");
+?>
+
 
 </body>
 </html>
--- static design/index.php
+++ static design/index.php
@@ -346,27 +346,9 @@ $_SESSION['rootPath'] = "./";
 
     </div>
 
-    <footer class="footer">
-        <div class="footer-container">
-            <div class=nav-container>
-                <a href="#" target="_blank">Home</a>
-                <a href="#" target="_blank">Program</a>
-                <a href="#" target="_blank">General</a>
-                <a href="#" target="_blank">League</a>
-                <a href="#" target="_blank">Teams</a>
-                <a href="#" target="_blank">Contact</a>
-                <a href="#" target="_blank">related links</a>
-                <a href="#" target="_blank">store</a>
-            </div>
-            <div class="social-icons-container">
-                <a href="#" class="fa fa-facebook"></a>
-                <a href="#" class="fa fa-twitter"></a>
-                <a href="#" class="fa fa-google"></a>
-                <a href="#" class="fa fa-youtube"></a>
-                <a href="#" class="fa fa-instagram"></a>
-            </div>
-        </div>
-    </footer>
+    <?php
+    include($_SESSION['rootPath'] . "assets/inc/footer.php");
+    ?>
 
 </div>
 
